4f647e4b7128a7d631d46653f731ffc1b7695c57
SatyaSailaja <dssailaja123@gmail.com>
2019-04-07 22:48:24 -0500

    Completed added background image need to add some styling

--- src/main/java/com/sailu/controllers/TakeAQuizController.java
+++ src/main/java/com/sailu/controllers/TakeAQuizController.java
@@ -7,12 +7,13 @@ import com.sailu.models.data.QuestionDao;
 import com.sailu.models.forms.Language;
 import com.sailu.models.forms.Question;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
 
 @Controller
 @RequestMapping("takeaquiz")
@@ -57,20 +58,42 @@ public class TakeAQuizController extends AbstractController {
     }
 
     @RequestMapping(value = "result", method = RequestMethod.POST)
-    public String  processViewQuestions(Model model) {
-
-
+    public String  processViewQuestions(Model model, @RequestParam HashMap<String,String> choices) {
 
         model.addAttribute("title", "Quiz Result");
 
+        HashMap<String,String> questionSets = new HashMap<String,String>();
+        List<String> quizQuestions = new ArrayList<String>();
 
-       /*for (String choice : choices) {
-            System.out.println("*******************************************************");
+        int count = 0;
 
-            System.out.println(choice);
 
+       for (String stringKey : choices.keySet()) {
+           // intIds.add(Integer.parseInt(stringKey));
+           Question questions = questionDao.findOne(Integer.parseInt(stringKey));
+           questionSets.put(String.valueOf(questions.getId()), questions.getAnswer());
+           quizQuestions.add(questions.getQuiz());
+           System.out.println(quizQuestions);
+       }
+
+       for (Map.Entry<String,String> entry : choices.entrySet()){
+            System.out.println("Key = " + entry.getKey() +
+                    ", Value = " + entry.getValue());
+            System.out.println("Actual Answer :"+questionSets.get(entry.getKey()));
+            if(questionSets.get(entry.getKey()).equals(entry.getValue())){
+                count++;
+
+            }
         }
-*/
+
+        model.addAttribute("count","Score :" + count +"/5");
+        model.addAttribute("choices",choices);
+        model.addAttribute("questionSets",questionSets);
+        model.addAttribute("quizQuestions",quizQuestions);
+
+
+
+
         return "takeaquiz/result";
     }
 
